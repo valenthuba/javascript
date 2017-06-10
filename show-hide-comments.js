e3bb3854db157c04ca5c36f532d35f613081dda8
valenthuba <valent.huba@gmail.com>
2017-06-10 20:07:29 +0900

    Add files via upload

---
+++ show-hide-comments.js
@@ -0,0 +1,2 @@
+// Show Hide Kolom Komentar Blogspot
+function showComm(e){document.getElementById(e)&&("none"!=document.getElementById(e+"-show").style.display?(document.getElementById(e+"-show").style.display="none",document.getElementById(e).style.display="block"):(document.getElementById(e+"-show").style.display="block",document.getElementById(e).style.display="none"))};
