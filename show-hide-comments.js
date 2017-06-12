4e5ecc0e3eab2cd62254fcacd8f33c149f6ababa
valenthuba <valent.huba@gmail.com>
2017-06-12 21:48:53 +0900

    Delete show-hide-comments.js

--- show-hide-comments.js
+++
@@ -1,2 +0,0 @@
-// Show Hide Kolom Komentar Blogspot
-function showComm(e){document.getElementById(e)&&("none"!=document.getElementById(e+"-show").style.display?(document.getElementById(e+"-show").style.display="none",document.getElementById(e).style.display="block"):(document.getElementById(e+"-show").style.display="block",document.getElementById(e).style.display="none"))};
